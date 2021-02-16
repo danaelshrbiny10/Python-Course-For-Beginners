b053c9910d354432b703c928e3ce10fda57d3586
danaelshrbiny10 <https://github.com/danaelshrbiny10/automated-price-comparison.git>
2021-02-16 03:39:16 +0200

    task2 lists & dictionary

---
+++ 2-lists & dictionary.py
@@ -0,0 +1,56 @@
+# q1 :
+Create a dictionary that contains the keys a  and b  and their respective values 1  and 2 .
+Hint: Dictionaries can be created either by using curly brackets or by using the dict  function.
+
+# --------------------------------------------------------------------------------------------------
+
+# q2 :
+Please complete the script so that it prints out the value of key b .
+d = {"a": 1, "b": 2}
+
+Expected output: 2
+
+# ----------------------------------------------------------------------------------------------------
+
+# q3 :
+Calculate the sum of the values of keys a  and b .
+d = {"a": 1, "b": 2, "c": 3}
+Expected output: 3
+
+# --------------------------------------------------------------------------------------------------
+
+# q4 :
+Add a new pair of key (e.g. c ) and value (e.g. 3 ) to the dictionary and print out the new dictionary.
+d = {"a": 1, "b": 2}
+Expected output: {'a': 1, 'c': 3, 'b': 2} 
+
+# --------------------------------------------------------------------------------------------------
+
+# q5:
+Complete the script so that it prints out the second item of the list.
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+Expected output: b
+
+# --------------------------------------------------------------------------------------------------
+
+# q6 :
+Complete the script so that it prints out letter i  using negative indexing.
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+Expected output: i 
+
+# --------------------------------------------------------------------------------------------------
+
+# q7:
+Complete the script so that it prints out a list slice containing the last three items of list letters .
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+Expected output: ['h', 'i', 'j'] 
+
+# --------------------------------------------------------------------------------------------------
+
+# q8:
+Complete the script so that it prints out a list slice containing letters a, c, e, g, and i. 
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+Expected output: ['a', 'c', 'e', 'g', 'i'] 
+
+# --------------------------------------------------------------------------------------------------
+
